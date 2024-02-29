fcfac3d7f795a4da3f0b60244d7d005e5f317bf3
Tanner Bebb <17tbebb@gmail.com>
2024-02-29 08:12:12 -0800

    Add project files.

---
+++ TannerBebbProj4/Main.asm
@@ -0,0 +1,16 @@
+;Tanner Bebb
+;Assembly Multiplication Practice
+
+.386
+Include Irvine32.inc 
+.stack 4096
+ExitProcess proto dwExitCode:dword
+
+.data
+
+.code
+
+main PROC
+
+main ENDP
+end main
