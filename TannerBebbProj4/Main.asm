8dc09354e8d0ee6cda0e927c22a17dc7186b4e02
Tanner Bebb <17tbebb@gmail.com>
2024-02-29 09:02:30 -0800

    I decided to restart the progam to test and see if I could print text to a screen.

--- TannerBebbProj4/Main.asm
+++ TannerBebbProj4/Main.asm
@@ -1,32 +1,23 @@
-;Tanner Bebb
-;Copy a string in reverse order
-
-.386
-.model flat, stdcall
-.stack 4096
-ExitProcess proto dwExitCode:dword
-Include Irvine32.inc 
-
+include Irvine32.inc
 
 .data
-
-source BYTE “Hello”, 0															;Declared string that will be reversed
-target BYTE SIZEOF source DUP(0)												;Declared variable that will be used as the loop counter.
+	source byte "Hello", 0
+	target byte ?
 
 .code
-
 main PROC
-
-	mov esi, 0																	;Moving the value of 0 into the esi register. Which is the instruction register.
-	mov eax, SIZEOF source														;This will be the loop counter.
-
+	mov esi, 0
+	mov ecx, lengthof source
 L1:
-	mov al, source[esi]															;Moving what value that is stored in the register into the al register.
-	mov target[esi], al															;Unsure as to what this does.
-	inc esi																		;Increment the esi register
-	loop L1																		;Loops back to the L1 tag and repeats the process
+	mov al, source[esi]
+	mov target[esi], al
+	inc esi
+	loop L1
+	mov edx, offset target
+	call writestring
 
-	invoke ExitProcess, 0
 
+	exit
 main ENDP
-end main
+
+END main
