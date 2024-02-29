ae7ac7f4ae2cc122a90533d8682a1bef7abd1747
Tanner Bebb <17tbebb@gmail.com>
2024-02-29 08:35:13 -0800

    General structure of the program has been started. I am running into build errors, but I am working on it.

--- TannerBebbProj4/Main.asm
+++ TannerBebbProj4/Main.asm
@@ -2,18 +2,31 @@
 ;Copy a string in reverse order
 
 .386
-Include Irvine32.inc 
+.model flat, stdcall
 .stack 4096
 ExitProcess proto dwExitCode:dword
+Include Irvine32.inc 
+
 
 .data
 
-source BYTE “This is the string that will be reversed”, 0						;Declared string that will be reversed
-target BYTE SIZEOF source DUP(0)												;Unsure right now.
+source BYTE “Hello”, 0															;Declared string that will be reversed
+target BYTE SIZEOF source DUP(0)												;Declared variable that will be used as the loop counter.
 
 .code
 
 main PROC
+
+	mov esi, 0																	;Moving the value of 0 into the esi register. Which is the instruction register.
+	mov eax, SIZEOF source														;This will be the loop counter.
+
+L1:
+	mov al, source[esi]															;Moving what value that is stored in the register into the al register.
+	mov target[esi], al															;Unsure as to what this does.
+	inc esi																		;Increment the esi register
+	loop L1																		;Loops back to the L1 tag and repeats the process
+
+	invoke ExitProcess, 0
 
 main ENDP
 end main
