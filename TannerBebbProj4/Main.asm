f9acccb60afce82e5835604b7e0e3c9ac811c039
Tanner Bebb <17tbebb@gmail.com>
2024-02-29 08:18:30 -0800

    Declared the two varibles needed for this project.

--- TannerBebbProj4/Main.asm
+++ TannerBebbProj4/Main.asm
@@ -1,5 +1,5 @@
 ;Tanner Bebb
-;Assembly Multiplication Practice
+;Copy a string in reverse order
 
 .386
 Include Irvine32.inc 
@@ -7,6 +7,9 @@ Include Irvine32.inc
 ExitProcess proto dwExitCode:dword
 
 .data
+
+source BYTE “This is the string that will be reversed”, 0						;Declared string that will be reversed
+target BYTE SIZEOF source DUP(0)												;Unsure right now.
 
 .code
 
