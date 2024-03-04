5ed9e41f9d11ead105089153994357c539f9c02d
Tanner Bebb <17tbebb@gmail.com>
2024-03-04 11:53:18 -0800

    Added documentation.

--- TannerBebbProj4/Main.asm
+++ TannerBebbProj4/Main.asm
@@ -1,20 +1,20 @@
 include Irvine32.inc
 
 .data
-	source byte "Hello", 0
-	target byte ?
+	source byte "Hello", 0										;This is the string that I am going to reverse.
+	target byte ?												;This will be used to go through each character in the string.
 
 .code
 main PROC
-	mov esi, 0
-	mov ecx, lengthof source
+	mov esi, 0													;Move the value of 0 to the esi register.
+	mov ecx, lengthof source									;Move the length of the source variable to the ecx register.
 L1:
-	mov al, source[esi]
-	mov target[esi], al
-	inc esi
-	loop L1
-	mov edx, offset target
-	call writestring
+	mov al, source[esi]											;Move the offset of source to the al register.
+	mov target[esi], al											;Move the al register into the offset of target.
+	inc esi														;Increment the esi register.
+	loop L1														;Jump back to the L1 tag and repeat the process.
+	mov edx, offset target										;Unsure.
+	call writestring											;Write the string to the screen.
 
 
 	exit
