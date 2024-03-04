5f6af687c68f3b502271d44380a6a3c44d9602bd
Tanner Bebb <17tbebb@gmail.com>
2024-03-04 12:01:42 -0800

    Reworked the program do to some testing.

--- TannerBebbProj4/Main.asm
+++ TannerBebbProj4/Main.asm
@@ -1,23 +1,26 @@
-include Irvine32.inc
+INCLUDE Irvine32.inc
+WriteString PROTO
 
-.data
-	source byte "Hello", 0										;This is the string that I am going to reverse.
-	target byte ?												;This will be used to go through each character in the string.
+    .data
 
-.code
-main PROC
-	mov esi, 0													;Move the value of 0 to the esi register.
-	mov ecx, lengthof source									;Move the length of the source variable to the ecx register.
-L1:
-	mov al, source[esi]											;Move the offset of source to the al register.
-	mov target[esi], al											;Move the al register into the offset of target.
-	inc esi														;Increment the esi register.
-	loop L1														;Jump back to the L1 tag and repeat the process.
-	mov edx, offset target										;Unsure.
-	call writestring											;Write the string to the screen.
+    source BYTE "This is the string that will be reversed",0
+    target BYTE SIZEOF source DUP('#')
 
+    .code
 
-	exit
-main ENDP
+    main PROC
+        mov esi,0
+        mov edi,LENGTHOF source - 2
+        mov ecx,SIZEOF source
+    L1:
+        mov al,source[esi]
+        mov target[edi],al
+        inc esi
+        dec edi
+        loop L1
+        mov edx, OFFSET target
+        call WriteString
 
-END main
+        exit
+    main ENDP
+    END main
